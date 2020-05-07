887922045b19ddfbfc304a61a232daf52cf6dd03
Steren Giannini <steren.giannini@gmail.com>
2020-05-06 22:04:11 -0700

    fix emulatemedia and do not start GUI

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14
+FROM node:14-slim
 
 RUN apt-get update && \
     # Adds required libs for Headless Chrome
--- htmltosvg.js
+++ htmltosvg.js
@@ -25,20 +25,21 @@ async function htmltosvg (req, res) {
 
     await page.goto(url);
     // https://github.com/puppeteer/puppeteer/blob/master/docs/api.md#pagepdfoptions
-    await page.emulateMediaType('screen');
+    await page.emulateMedia('screen');
     await page.pdf({
         path: inputPDFFilename,
         format: 'A3',
         printBackground: true
     });
 
-    child.exec(`inkscape --file ${inputPDFFilename} --export-plain-svg ${outputSVGFilename}`, async (error, stdout, stderr) => {
+    // https://inkscape.org/doc/inkscape-man.html
+    // When available (Inkscape 1.0), shuld we use --pdf-poppler for better fidelity? text would be transformed to path  https://gitlab.com/inkscape/inkscape/-/issues/263
+    child.exec(`inkscape --without-gui --file ${inputPDFFilename} --export-plain-svg ${outputSVGFilename}`, async (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             res.status(500)
             return res.send(`error: ${error.message}`);
         }
-        console.log(`stdout: ${stdout}`);
         const outputSVG = await readFile(outputSVGFilename);
         res.set('Content-Type', 'image/svg+xml');
         res.send(outputSVG);
