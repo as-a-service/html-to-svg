ea18681f7808b5d19962bf60815459ce7197bf65
Steren Giannini <steren.giannini@gmail.com>
2020-05-06 20:51:44 -0700

    Return PDF of webpage

--- htmltosvg.js
+++ htmltosvg.js
@@ -1,31 +1,29 @@
 import puppeteer from 'puppeteer';
 
-/**
- * HTTP Cloud Function.
- * This function is exported by index.js, and is executed when
- * you make an HTTP request to the deployed function's endpoint.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
+let browser;
+let page;
+
 async function htmltosvg (req, res) {
     const url = req.query.url;
-  
-    if (!url) {
+      if (!url) {
       return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
     }
-  
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-    const page = await browser.newPage();
+
+    if(!browser) {
+        browser = await puppeteer.launch({
+            args: ['--no-sandbox']
+        });
+    }
+    if(!page) {
+        page = await browser.newPage();
+    }
+
     await page.goto(url);
-    const imageBuffer = await page.screenshot();
-    browser.close();
-  
-    res.set('Content-Type', 'image/png');
-    res.send(imageBuffer);
+    const pdf = await page.pdf({ format: 'A4' });
+
+    res.set('Content-Type', 'application/pdf');
+    res.send(pdf);
   };
 
-  export {htmltosvg};
+export {htmltosvg};
   
